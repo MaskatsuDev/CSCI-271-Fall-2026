d8f1138184e1b3a8c19b82923956fd1d2236c073
Angelo <angelo.eustache123@gmail.com>
2026-09-07 13:06:34 -0400

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string NameRequest;
+
+    cout <<"Hello World" << endl;
+    cout << "What is your name? ";
+    cin >> NameRequest;
+
+    cout << "Nice to meet you, " << NameRequest << "!" << endl;
+    
+    return 0;
+}
+
+
